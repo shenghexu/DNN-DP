81f788fd2e09549b82b9c097067fa7a6bf451304
david xu <shenghexu@gmail.com>
2020-02-20 18:17:23 -0500

    Update

--- NDP_TSP_Policy/NDP_Policy_20/DNN_model_4.py
+++ NDP_TSP_Policy/NDP_Policy_20/DNN_model_4.py
@@ -10,10 +10,6 @@ class DP_M(nn.Module):
     def __init__(self):
         super(DP_M, self).__init__()
         self.fc1    = nn.Linear(16, 2, bias=False)
-        #bp()
-        #K = torch.Tensor([[0.0, 1.0, 0.0, 0.0,  0.0,  ], 
-        #    [0.0, 1.0, 1.0, 0.0]])
-        #K = torch.unsqueeze(K, 0)
 
         K = torch.zeros(2, 16)
         K[0, 1]     = 1.0
@@ -27,16 +23,9 @@ class DP_M(nn.Module):
 
     def forward(self, x):
         Batch_size = x.shape[0]
-        #bp()
         out = self.fc1(x)
-        #bp()
-        #out = self.relu(out)
-        #out = self.fc2(out)
-        #out = self.relu(out)
-        #out = self.fc3(out)
         indices = torch.argmin(out, dim=1)
         
-        #values = x[0:Batch_size, indices]
         
         return out, indices
 
